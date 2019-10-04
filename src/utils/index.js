af8691aed84f6a6e4c2684e5816685bab07d3dde
Chris Decker <chris.decker@volusion.com>
2019-10-04 14:22:48 -0500

    refactor: Extracts responsive nesting to helper fn

---
+++ src/utils/index.js
@@ -0,0 +1,20 @@
+export const createResponsiveClasses = (cssObj = { yourClassName: {} }) => {
+    const className = Object.keys(cssObj)[0];
+    const responsiveClasses = {};
+    const passedCss = cssObj[className];
+    responsiveClasses[className] = passedCss;
+    const notSmallClassName = `${className}-ns`;
+    const notSmallBreakpoint = '@media only screen and (min-width: 30em)';
+    const mediumClassName = `${className}-m`;
+    const mediumBreakpoint =
+        '@media only screen and (min-width: 30em) and (max-width: 60em)';
+    const largeClassName = `${className}-l`;
+    const largeBreakpoint = '@media only screen and (min-width: 60em)';
+    responsiveClasses[notSmallClassName] = {};
+    responsiveClasses[notSmallClassName][notSmallBreakpoint] = passedCss;
+    responsiveClasses[mediumClassName] = {};
+    responsiveClasses[mediumClassName][mediumBreakpoint] = passedCss;
+    responsiveClasses[largeClassName] = {};
+    responsiveClasses[largeClassName][largeBreakpoint] = passedCss;
+    return responsiveClasses;
+};
--- src/width.js
+++ src/width.js
@@ -1,14 +1,5 @@
+import { createResponsiveClasses } from './utils';
+
 export const width = {
-    'w-100': { width: '100%' },
-    'w-100-ns': {
-        '@media only screen and (min-width: 30em)': { width: '100%' }
-    },
-    'w-100-m': {
-        '@media only screen and (min-width: 30em) and (max-width: 60em)': {
-            width: '100%'
-        }
-    },
-    'w-100-l': {
-        '@media only screen and (min-width: 60em)': { width: '100%' }
-    }
+    ...createResponsiveClasses({ 'w-100': { width: '100%' } })
 };
