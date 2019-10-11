a61623ba5f0c0d4a25119fd4f2a91cd1e2b580df
Chris Decker <chris.decker@volusion.com>
2019-10-11 11:13:46 -0500

    fix(forms): Corrects classNames.

--- __tests__/forms.spec.js
+++ __tests__/forms.spec.js
@@ -1,16 +1,18 @@
 import { cssForClass } from './utils';
 
 describe('The aphroditeTachyons object', () => {
-    it('should contain the correct forms classes', () => {
+    const mozFocusCssRules = {
+        '::-moz-focus-inner': { border: 0, padding: 0 }
+    };
+    it('should contain the correct input-reset class', () => {
         const cssRules = {
             '-webkit-appearance': 'none',
-            '-moz-appearance': 'none'
+            '-moz-appearance': 'none',
+            ...mozFocusCssRules
         };
         expect(cssForClass('input-reset')).toEqual(cssRules);
     });
-    it('should contain the correct forms classes', () => {
-        const cssRules = { border: 0, padding: 0 };
-        expect(cssForClass('button-reset::-moz-focus-inner')).toEqual(cssRules);
-        expect(cssForClass('input-reset::-moz-focus-inner')).toEqual(cssRules);
+    it('should contain the correct button-reset class', () => {
+        expect(cssForClass('button-reset')).toEqual(mozFocusCssRules);
     });
 });
--- src/properties/forms.js
+++ src/properties/forms.js
@@ -1,6 +1,9 @@
-const resetCss = { border: 0, padding: 0 };
+const resetCss = { '::-moz-focus-inner': { border: 0, padding: 0 } };
 export const forms = {
-    'input-reset': { '-webkit-appearance': 'none', '-moz-appearance': 'none' },
-    'button-reset::-moz-focus-inner': resetCss,
-    'input-reset::-moz-focus-inner': resetCss
+    'input-reset': {
+        '-webkit-appearance': 'none',
+        '-moz-appearance': 'none',
+        ...resetCss
+    },
+    'button-reset': resetCss
 };
