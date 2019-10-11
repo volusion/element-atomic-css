36b195f3e1f31d3b8538e9e232fce7bd03ae12cf
Chris Decker <chris.decker@volusion.com>
2019-10-11 14:11:49 -0500

    test: Removes old verbose tests.

--- __tests__/utils/index.js
+++
@@ -1,7 +0,0 @@
-import { StyleSheet } from 'aphrodite';
-import { aphroditeTachyons } from '../../src';
-
-const classes = StyleSheet.create(aphroditeTachyons);
-const css = '_definition';
-
-export const cssForClass = className => classes[className][css];
