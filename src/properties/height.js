ffd670090c49997fcc39b25f7615c8ecad7705ac
Chris Decker <chris.decker@volusion.com>
2019-10-17 10:31:56 -0500

    fix: Corrects height classes.
    - Removes `-ns` suffix that shouldn’t be on the default classes.

--- src/properties/height.js
+++ src/properties/height.js
@@ -1,23 +1,23 @@
 import { createResponsiveClasses } from '../utils';
 
 const classes = {
-    ...createResponsiveClasses({ 'h1-ns': { height: '1rem' } }),
-    ...createResponsiveClasses({ 'h2-ns': { height: '2rem' } }),
-    ...createResponsiveClasses({ 'h3-ns': { height: '4rem' } }),
-    ...createResponsiveClasses({ 'h4-ns': { height: '8rem' } }),
-    ...createResponsiveClasses({ 'h5-ns': { height: '16rem' } }),
-    ...createResponsiveClasses({ 'h-25-ns': { height: '25%' } }),
-    ...createResponsiveClasses({ 'h-50-ns': { height: '50%' } }),
-    ...createResponsiveClasses({ 'h-75-ns': { height: '75%' } }),
-    ...createResponsiveClasses({ 'h-100-ns': { height: '100%' } }),
-    ...createResponsiveClasses({ 'min-h-100-ns': { minHeight: '100%' } }),
-    ...createResponsiveClasses({ 'vh-25-ns': { height: '25vh' } }),
-    ...createResponsiveClasses({ 'vh-50-ns': { height: '50vh' } }),
-    ...createResponsiveClasses({ 'vh-75-ns': { height: '75vh' } }),
-    ...createResponsiveClasses({ 'vh-100-ns': { height: '100vh' } }),
-    ...createResponsiveClasses({ 'min-vh-100-ns': { minHeight: '100vh' } }),
-    ...createResponsiveClasses({ 'h-auto-ns': { height: 'auto' } }),
-    ...createResponsiveClasses({ 'h-inherit-ns': { height: 'inherit' } })
+    ...createResponsiveClasses({ h1: { height: '1rem' } }),
+    ...createResponsiveClasses({ h2: { height: '2rem' } }),
+    ...createResponsiveClasses({ h3: { height: '4rem' } }),
+    ...createResponsiveClasses({ h4: { height: '8rem' } }),
+    ...createResponsiveClasses({ h5: { height: '16rem' } }),
+    ...createResponsiveClasses({ 'h-25': { height: '25%' } }),
+    ...createResponsiveClasses({ 'h-50': { height: '50%' } }),
+    ...createResponsiveClasses({ 'h-75': { height: '75%' } }),
+    ...createResponsiveClasses({ 'h-100': { height: '100%' } }),
+    ...createResponsiveClasses({ 'min-h-100': { minHeight: '100%' } }),
+    ...createResponsiveClasses({ 'vh-25': { height: '25vh' } }),
+    ...createResponsiveClasses({ 'vh-50': { height: '50vh' } }),
+    ...createResponsiveClasses({ 'vh-75': { height: '75vh' } }),
+    ...createResponsiveClasses({ 'vh-100': { height: '100vh' } }),
+    ...createResponsiveClasses({ 'min-vh-100': { minHeight: '100vh' } }),
+    ...createResponsiveClasses({ 'h-auto': { height: 'auto' } }),
+    ...createResponsiveClasses({ 'h-inherit': { height: 'inherit' } })
 };
 
 export default classes;
