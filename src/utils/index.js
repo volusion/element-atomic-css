a81be6ea740329fd568c9ac727829fa7b416dc37
Chris Decker <chris.decker@volusion.com>
2019-10-18 16:56:25 -0500

    chore: Updates export to use default

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
-export { createResponsiveClasses } from './createResponsiveClasses';
+export { default as createResponsiveClasses } from './createResponsiveClasses';
 export { default as joinClasses } from './joinClasses';
 export { default as atomic } from './atomic';
