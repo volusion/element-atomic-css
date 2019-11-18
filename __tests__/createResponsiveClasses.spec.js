2f38ec503e9543cbcd988a5df8589ff03049fbc1
Chris Decker <chris.decker@volusion.com>
2019-11-18 14:37:51 -0600

    refactor(createResponsiveClasses): Parses multiple args
    - Allows the `createResponsiveClasses` function to parse multiple arguments passed to it.

--- __tests__/createResponsiveClasses.spec.js
+++ __tests__/createResponsiveClasses.spec.js
@@ -34,4 +34,20 @@ describe('The createResponsiveClasses function', () => {
             })
         );
     });
+    describe('when more than one argument is passed', () => {
+        it('should return the suffixed classes for each argument', () => {
+            const responsiveClasses = createResponsiveClasses(
+                { db: { display: 'block' } },
+                { dib: { display: 'inline-block' } }
+            );
+            expect(responsiveClasses['db']).toBeDefined();
+            expect(responsiveClasses['db-ns']).toBeDefined();
+            expect(responsiveClasses['db-m']).toBeDefined();
+            expect(responsiveClasses['db-l']).toBeDefined();
+            expect(responsiveClasses['dib']).toBeDefined();
+            expect(responsiveClasses['dib-ns']).toBeDefined();
+            expect(responsiveClasses['dib-m']).toBeDefined();
+            expect(responsiveClasses['dib-l']).toBeDefined();
+        });
+    });
 });
